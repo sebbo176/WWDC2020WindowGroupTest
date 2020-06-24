a7668b5158b0ff6873b79ee11d79746297f44704
Sebastian Bolling <sebastian.bolling@bookbeat.com>
2020-06-25 00:40:45 +0200

    Initial Commit

---
+++ Shared/WWDC2020MultipleWindowsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC2020MultipleWindowsAppApp.swift
+//  Shared
+//
+//  Created by Sebastian Bolling on 2020-06-25.
+//
+
+import SwiftUI
+
+@main
+struct WWDC2020MultipleWindowsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
